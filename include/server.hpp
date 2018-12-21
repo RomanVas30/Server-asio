d3a464fdef8f2a07be64b08734ca30e011920d62
Roman <romanvas3008@gmail.com>
2018-12-21 08:28:54 +0000

    add server

--- include/server.hpp
+++ include/server.hpp
@@ -4,104 +4,55 @@
 #define INCLUDE_SERVER_HPP_
 
 #include <algorithm>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/config.hpp>
+#include <boost/detail/iterator.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/thread.hpp>
+#include <chrono>
+#include <cstdlib>
 #include <iostream>
 #include <memory>
 #include <string>
-#include <cstdlib>
-#include <chrono>
 #include <vector>
-#include <boost/thread.hpp>
-#include <boost/bind.hpp>
-#include <boost/asio.hpp>
-#include <boost/shared_ptr.hpp>
-#include <boost/enable_shared_from_this.hpp>
 
-//using namespace boost::gregorian;
-using namespace boost::posix_time;
 using namespace boost::asio;
+using namespace boost::posix_time;
 
 class talk_to_client {
 public:
-  talk_to_client(){}
-  std::string username() const { return username_; }
-  void answer_to_client(){
-    try{
-      read_request();
-      process_request();
+    talk_to_client() : sock_(service) {}
+    std::string username() const {
+        return username_;
     }
-    catch ( boost::system::system_error&){
-      stop();
-    }
-    if ( timed_out())
-      stop();
-    }
-  void set_clients_changed() { clients_changed_ = true; }
-  ip::tcp::socket & sock() { return sock_; }
-  bool timed_out() const{
-    ptime now = microsec_clock::local_time();
-    long long ms = (now - last_ping).total_milliseconds();
-    return ms > 5000 ;
-  }
-  void stop(){
-    boost::system::error_code err; sock_.close(err);
-  }
-  void read_request(){
-    if ( sock_.available())
-      already_read_ += sock_.read_some(
-      buffer(buff_ + already_read_, max_msg - already_read_));
-  }
-  void process_request()
-{
-	bool found_enter = std::find(buff_, buff_ + already_read_, '\n') < buff_ + already_read_;
-	if ( !found_enter)
-		return; // message is not full
-	// process the msg
-	last_ping = microsec_clock::local_time();
-	size_t pos = std::find(buff_, buff_ + already_read_, '\n') - buff_;
-	std::string msg(buff_, pos);
-	std::copy(buff_ + already_read_, buff_ + max_msg, buff_);
-	already_read_ -= pos + 1;
-	if ( msg.find("login ") == 0) on_login(msg);
-	else if ( msg.find("ping") == 0) on_ping();
-	else if ( msg.find("ask_clients") == 0) on_clients();
-	else std::cerr << "invalid msg " << msg << std::endl;
-}
-void on_login(const std::string & msg)
-{
-	std::istringstream in(msg);
-	in >> username_ >> username_;
-	write("login ok\n");
-	update_clients_changed();
-}
-void on_ping()
-{
-	write(clients_changed_ ? "ping client_list_changed\n" : "ping ok\n");
-	clients_changed_ = false;
-}
-void on_clients()
-{
-	std::string msg;
-	{
-		boost::recursive_mutex::scoped_lock lk(cs);
-		for( array::const_iterator b = clients.begin(), e = clients.end() ;b != e; ++b)
-			msg += (*b)->username() + " ";
-	}
-	write("clients " + msg + "\n");
-}
-void write(const std::string & msg) { sock_.write_some(buffer(msg)); }
+    void answer_to_client();
+    void set_clients_changed();
+    void stop();
+    bool timed_out() const;
+    ip::tcp::socket& sock();
+    void read_request();
+    void process_request();
+    void on_login(const std::string & msg);
+    void on_ping();
+    void on_clients();
+    void write(const std::string & msg);
+    void accept_thread();
+    void handle_clients_thread();
 
 private:
-	io_service service;
-	std::vector<std::shared_ptr<talk_to_client>> clients;
-	boost::recursive_mutex mutex;
-	ip::tcp::socket sock_;
-	enum { max_msg = 1024 };
-	int already_read_;
-	char buff_[max_msg];
-	bool started_;
-	std::string username_;
-	bool clients_changed_;
-	ptime last_ping;
+    io_service service;
+    std::vector<boost::shared_ptr<talk_to_client>> clients;
+    boost::recursive_mutex mutex;
+    ip::tcp::socket sock_;
+    enum { max_msg = 1024 };
+    char buff_[max_msg];
+    bool started_;
+    std::string username_;
+    bool clients_changed_;
+    ptime last_ping;
 };
 
 #endif // INCLUDE_SERVER_HPP_
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,33 +1,12 @@
 #include <server.hpp>
 
-void accept_thread()
+int main()
 {
-ip::tcp::acceptor acceptor(service, ip::tcp::endpoint(ip::tcp::v4(), 8001));
-while ( true)
-{
-	client_ptr new_( new talk_to_client);
-	acceptor.accept(new_->sock());
-	boost::recursive_mutex::scoped_lock lk(cs);
-	clients.push_back(new_);
-}
-}
-void handle_clients_thread()
-{
-	while ( true)
-	{
-		boost::this_thread::sleep( millisec(1));
-		boost::recursive_mutex::scoped_lock lk(cs);
-		for(array::iterator b = clients.begin(),e = clients.end(); b != e; ++b)
-		(*b)->answer_to_client();
-		// erase clients that timed out
-		clients.erase(std::remove_if(clients.begin(), clients.end(),
-		boost::bind(&talk_to_client::timed_out,_1)), clients.end());
-	}
-}
-int main(int argc, char* argv[])
-{
-	boost::thread_group threads;
-	threads.create_thread(accept_thread);
-	threads.create_thread(handle_clients_thread);
-	threads.join_all();
+    auto talk = boost::make_shared<talk_to_client>();
+    /*boost::thread_group threads;
+    threads.create_thread(&talk_to_client::accept_thread, talk);
+    threads.create_thread(&talk_to_client::handle_clients_thread, talk);
+    threads.join_all();*/
+    std::thread{&talk_to_client::accept_thread, talk}.join();
+    std::thread{&talk_to_client::handle_clients_thread, talk}.join();
 }
